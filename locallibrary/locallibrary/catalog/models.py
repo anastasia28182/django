be201c96c3c66467a402dbac8398bd5976132a01
gr433_koadm <anastasia.korol281@gmail.com>
2024-12-11 13:14:28 +0700

    8-9

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -1,111 +1,75 @@
-import uuid # Required for unique book instances
 from django.db import models
-from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+from django.urls import reverse
+import uuid
 from django.contrib.auth.models import User
 from datetime import date
 
 class Genre(models.Model):
-    """
-    Model representing a book genre (e.g. Science Fiction, Non Fiction).
-    """
-    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    name = models.CharField(max_length=200, help_text="Укажите жанр книги (например, научная фантастика, французская поэзия и т. д.)", verbose_name=("Жанр"))
 
     def __str__(self):
-        """
-        String for representing the Model object (in Admin site etc.)
-        """
         return self.name
 
-
-
-
 class Book(models.Model):
-    """
-    Model representing a book (but not a specific copy of a book).
-    """
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    title = models.CharField(max_length=200, verbose_name=("Название"))
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name=("Автор"))
+    summary = models.TextField(max_length=1000, help_text="Введите краткое описание книги", verbose_name=("Описание"))
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text="Выберите жанр(ы) книги", verbose_name=("Жанр"))
 
     def __str__(self):
-        """
-        String for representing the Model object.
-        """
         return self.title
 
+
     def get_absolute_url(self):
-        """
-        Returns the url to access a particular book instance.
-        """
         return reverse('book-detail', args=[str(self.id)])
 
     def display_genre(self):
-        """
-        Creates a string for the Genre. This is required to display genre in Admin.
-        """
-        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
     display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Уникальный ID этой конкретной книги во всей библиотеке.", verbose_name=("ID"))
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True, verbose_name=("Книга"))
+    imprint = models.CharField(max_length=200, verbose_name=("Штамп(год выпуска)"))
+    due_back = models.DateField(null=True, blank=True, verbose_name=("Дата возвращения книги"))
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=("Заёмщик"))
 
     LOAN_STATUS = (
-        ('m', 'Обслуживание'),
-        ('o', 'В долг'),
-        ('a', 'Доступный'),
-        ('r', 'Зарезервированный'),
+        ('m', 'Книга на тех. обслуживании'),
+        ('o', 'Взята'),
+        ('a', 'Доступна'),
+        ('r', 'Зарезервирована'),
     )
 
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Статус книги', verbose_name=("Статус"))
 
     class Meta:
         ordering = ["due_back"]
-        permissions = (("can_mark_returned", "Установить книгу как возвращенную"),)
 
-    def str(self):
-        """
-        String for representing the Model object
-        """
+
+    def __str__(self):
         return '%s (%s)' % (self.id,self.book.title)
 
-    @property
     def is_overdue(self):
         if self.due_back and date.today() > self.due_back:
             return True
         return False
 
 class Author(models.Model):
-    """
-    Model representing an author.
-    """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    first_name = models.CharField(max_length=100, verbose_name=("Имя"))
+    last_name = models.CharField(max_length=100, verbose_name=("Фамилия"))
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name=("Дата рождения"))
+    date_of_death = models.DateField('Умер', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
-        """
-        Returns the url to access a particular author instance.
-        """
         return reverse('author-detail', args=[str(self.id)])
 
 
     def __str__(self):
-        """
-        String for representing the Model object.
-        """
         return '%s, %s' % (self.last_name, self.first_name)
 
